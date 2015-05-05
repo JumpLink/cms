9cd6be6f5edd8fdedf4c74821221b4a465e2d391
JumpLink <pascal@jumplink.eu>
2015-05-05 14:44:18 +0200

    use config/local.json instead of config/local.js for custom local configurations and check if this file exists

--- cms.js
+++ cms.js
@@ -22,18 +22,41 @@
 // no matter where we actually lift from.
 process.chdir(__dirname);
 
-// Ensure a "sails" can be located:
-(function() {
+
+var fs = require('fs');
+// var spawn = require('child_process').spawn;
+var path = require('path');
+var log = require('captains-log')();
+var platform = path.basename(process.argv[0]); // iojs, node, nodejs, etc
+
+fs.exists('config/local.json', function(exists) { 
+  if (!exists) {
+    log.warn("Your config/local.json is missing, start configuration webapp..");
+    startConfig();
+  } else {
+    startSails();
+  }
+});
+
+var startSails = function () {
+  // Ensure a "local.json" can be loaded:
+  try {
+    var local = require('./config/local.json');
+  } catch (e) {
+    log.error('Your config/local.json ist corrupt or missing!');
+    return;
+  }
+  // Ensure a "sails" can be located:
   var sails;
   try {
     sails = require('sails');
   } catch (e) {
-    console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
-    console.error('To do that, run `npm install sails`');
-    console.error('');
-    console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
-    console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
-    console.error('but if it doesn\'t, the app will run with the global sails instead!');
+    log.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
+    log.error('To do that, run `npm install sails`');
+    log.error('');
+    log.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
+    log.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
+    log.error('but if it doesn\'t, the app will run with the global sails instead!');
     return;
   }
 
@@ -45,10 +68,10 @@ process.chdir(__dirname);
     try {
       rc = require('sails/node_modules/rc');
     } catch (e1) {
-      console.error('Could not find dependency: `rc`.');
-      console.error('Your `.sailsrc` file(s) will be ignored.');
-      console.error('To resolve this, run:');
-      console.error('npm install rc --save');
+      log.error('Could not find dependency: `rc`.');
+      log.error('Your `.sailsrc` file(s) will be ignored.');
+      log.error('To resolve this, run:');
+      log.error('npm install rc --save');
       rc = function () { return {}; };
     }
   }
@@ -56,4 +79,8 @@ process.chdir(__dirname);
 
   // Start server
   sails.lift(rc('sails'));
-})();
+}
+
+var startConfig = function () {
+  log.error("TODO");
+}
