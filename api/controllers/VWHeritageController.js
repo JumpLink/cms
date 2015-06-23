60235848abac60a4c9a73da642a7f7477ddd6397
JumpLink <pascal@jumplink.eu>
2015-06-23 22:55:37 +0200

    get vwh info and images

--- api/controllers/VWHeritageController.js
+++ api/controllers/VWHeritageController.js
@@ -30,12 +30,32 @@ module.exports = {
     MultisiteService.getCurrentSiteConfig(req.session.uri.host, function (err, config) {
       if(err) { sails.log.error(err); return res.serverError(err); }
       var vwheritage = require('heritage')(config.vwheritage);
-      var sku = req.param('sku');
+      var id = req.param('id'); // can also be the id
 
       if(err) { sails.log.error(err); return res.serverError(err); }
-      vwheritage.xmlrpc.auto.catalog.product.info(sku, function (err, result) {
+      vwheritage.auto.catalog.product.infoById(id, function (err, result) {
         if(err) { sails.log.error(err); return res.serverError(err); }
         sails.log.debug(result);
+        res.json(result);
+      });
+    });
+  },
+
+  catalog_product_images: function (req, res, next) {
+
+    sails.log.debug("vwheritage/catalog_product_images");
+
+    MultisiteService.getCurrentSiteConfig(req.session.uri.host, function (err, config) {
+      if(err) { sails.log.error(err); return res.serverError(err); }
+      var vwheritage = require('heritage')(config.vwheritage);
+      var id = req.param('id');
+
+      sails.log.debug("[VWHeritageController]", "id", id);
+
+      if(err) { sails.log.error(err); return res.serverError(err); }
+      vwheritage.auto.catalog.product.images(id, function (err, result) {
+        if(err) { sails.log.error("[VWHeritageController]", err); return res.serverError(err); }
+        sails.log.debug("[VWHeritageController]", result);
         res.json(result);
       });
     });
@@ -51,7 +71,7 @@ module.exports = {
       var skus = req.param('skus');
 
       if(err) { sails.log.error(err); return res.serverError(err); }
-      vwheritage.xmlrpc.auto.catalog.product.info(skus, function (err, result) {
+      vwheritage.auto.catalog.product.info(skus, function (err, result) {
         if(err) { sails.log.error(err); return res.serverError(err); }
         sails.log.debug(result);
         res.json(result);
